9f83ee027f7001986deef14fcdbbbbf897d0464a
Oriol Furnells Sanchez <oriol.furnells@gmail.com>
2018-12-18 17:46:11 +0100

    senado18/12

---
+++ JS/home.js
@@ -0,0 +1,9 @@
+//pdte cambiar nombre
+function expand() {
+    if (innerHTML == "Expand") {
+        document.getElementById("expand").innerHTML = "Contract";
+    } else {
+        document.getElementById("expand").innerHTML = "Expand";
+    }
+}
+
